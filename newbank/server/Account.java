08ef40b12f3bd0765287f96ae70bc2a49394f555
metajos <101225895+metajos@users.noreply.github.com>
2022-03-13 10:14:57 +0000

    Add files via upload

--- newbank/server/Account.java
+++ newbank/server/Account.java
@@ -10,6 +10,9 @@ public class Account {
 		this.openingBalance = openingBalance;
 	}
 	
+	public String getName(){
+		return accountName;
+	}
 	public String toString() {
 		return (accountName + ": " + openingBalance);
 	}
--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -3,35 +3,39 @@ package newbank.server;
 import java.util.HashMap;
 
 public class NewBank {
-	
+
 	private static final NewBank bank = new NewBank();
-	private HashMap<String,Customer> customers;
-	
+	private HashMap<String, Customer> customers;
+
 	private NewBank() {
 		customers = new HashMap<>();
 		addTestData();
 	}
-	
+
 	private void addTestData() {
 		Customer bhagy = new Customer();
 		bhagy.addAccount(new Account("Main", 1000.0));
 		customers.put("Bhagy", bhagy);
-		
+		newAccount(bhagy, "Investment Account");
+
 		Customer christina = new Customer();
 		christina.addAccount(new Account("Savings", 1500.0));
 		customers.put("Christina", christina);
-		
+		newAccount(christina, "Holiday Fund");
+
 		Customer john = new Customer();
 		john.addAccount(new Account("Checking", 250.0));
 		customers.put("John", john);
+		newAccount(john, "Savings");
+
 	}
-	
+
 	public static NewBank getBank() {
 		return bank;
 	}
-	
+
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
-		if(customers.containsKey(userName)) {
+		if (customers.containsKey(userName)) {
 			return new CustomerID(userName);
 		}
 		return null;
@@ -39,17 +43,31 @@ public class NewBank {
 
 	// commands from the NewBank customer are processed in this method
 	public synchronized String processRequest(CustomerID customer, String request) {
-		if(customers.containsKey(customer.getKey())) {
-			switch(request) {
-			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
-			default : return "FAIL";
+		if (customers.containsKey(customer.getKey())) {
+			switch (request) {
+				case "SHOWMYACCOUNTS":
+					return showMyAccounts(customer);
+				default:
+					return "FAIL";
+				case "NEWACCOUNT":
+					return newAccount(customer, request);
+
 			}
 		}
 		return "FAIL";
 	}
-	
+
 	private String showMyAccounts(CustomerID customer) {
 		return (customers.get(customer.getKey())).accountsToString();
+	}
+
+	private void newAccount(CustomerID customer, String name) {
+		if (customer.isNewAccountNameValid(name)) {
+			customer.addAccount(new Account(name, 0));
+			System.out.println("SUCCESS");
+		} else {
+			System.out.println("FAIL");
+		}
 	}
 
 }
